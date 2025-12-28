cd4fbca2dcc30f93bc33f3f69371f9f2b5de1361
Perlou <perloukevin@gmail.com>
2025-12-28 10:00:02 +0800

    feat: init

---
+++ src/main/java/com/example/jp_order_management/JpOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.jp_order_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpOrderManagementApplication.class, args);
+	}
+
+}
